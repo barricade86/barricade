b0db9d695e6a8df75f06988f82b8fb0e550740d6
barricade <shilov.kirill.transas@gmail.com>
2015-02-23 18:36:07 +0300

    Auth realised

--- lesson2/views/auth_form.php
+++ lesson2/views/auth_form.php
@@ -16,7 +16,7 @@
                 <title>Аутентификация</title>
             </head>
             <body>
-            <p><b>Войдите на сайт под своим именем для получения расширенных возможностей:</b></p>
+            <p><b>Войдите на сайт под своим именем для получения возможности администрирования ленты:</b></p>
             <form action="./components/login.php" method="POST">
                 <label for="login">логин:</label><input type="text" name="login" required>
                 <label for="pass">пароль:</label><input type="password" name="pass" required>
